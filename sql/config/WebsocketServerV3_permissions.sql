3a7a3e427fb9e31173fe425185c4a79b9477e701
bshaw <bshawvt@gmail.com>
2019-09-13 03:28:45 -0400

    restructure & comments

--- sql/config/WebsocketServerV3_permissions.sql
+++ sql/config/WebsocketServerV3_permissions.sql
@@ -1,12 +1,16 @@
 # grant user access to our stored functions and procedures
+use wss_debug;
+
 #GRANT EXECUTE ON FUNCTION wss_debug.WSService_MakeHash TO 'web'@'%';
 #REVOKE EXECUTE ON FUNCTION wss_debug.WSService_MakeHash FROM 'web'@'%';
 
+#website
 GRANT EXECUTE ON PROCEDURE wss_debug.WSProc_SelectUserAccountOnAuth TO 'wss_website'@'%';
 #REVOKE EXECUTE ON PROCEDURE wss_debug.WSProc_SelectUserAccountOnAuth FROM 'web'@'%';
 
 GRANT EXECUTE ON PROCEDURE wss_debug.WSProc_GenerateTokenOnAuth TO 'wss_website'@'%';
 #REVOKE EXECUTE ON PROCEDURE wss_debug.WSProc_GenerateTokenOnAuth FROM 'web'@'%';
 
-#GRANT EXECUTE ON PROCEDURE wss_debug.WSProc_ConsumeToken TO 'web'@'%';
-#REVOKE EXECUTE ON PROCEDURE wss_debug.WSProc_ConsumeToken FROM 'web'@'%';
+#server
+GRANT EXECUTE ON PROCEDURE wss_debug.WSProc_ConsumeToken TO 'server'@'%';
+#REVOKE EXECUTE ON PROCEDURE wss_debug.WSProc_ConsumeToken FROM 'server'@'%';
--- sql/config/WebsocketServerV3_tabledefs.sql
+++ sql/config/WebsocketServerV3_tabledefs.sql
@@ -1,15 +1,19 @@
 create database if not exists wss_debug;
 use wss_debug;
 
+drop table if exists useraccountrecovery;
+drop table if exists useraccounts;
 
-#drop table if exists activesessions;
-#drop table if exists useraccountrecovery;
-#drop table if exists useraccounts;
 #drop table if exists serverstatus;
 #drop table if exists characters;
 #drop table if exists items;
 #drop table if exists actions;
 
+drop table if exists forum;
+drop table if exists thread;
+drop table if exists content;
+
+# 
 CREATE TABLE IF NOT EXISTS forum (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT,
     name VARCHAR(64),
@@ -17,11 +21,13 @@ CREATE TABLE IF NOT EXISTS forum (
     
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS thread (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT,
     userId BIGINT(20) NOT NULL, # useraccount id
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS content (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT,
     threadId BIGINT(20) NOT NULL,
@@ -30,27 +36,25 @@ CREATE TABLE IF NOT EXISTS content (
     PRIMARY KEY (id)
 );
 
-
-
-
+# 
 CREATE TABLE IF NOT EXISTS useraccounts (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT, # primary key
-	username VARCHAR(18) NOT NULL, # username
-	combinedHash BLOB NOT NULL, # hash
-	salt BLOB NOT NULL, # a random salt
-    email VARCHAR(128) NOT NULL, # email 
-    permission INT DEFAULT 0, # permissions given to this
-    locked BOOL DEFAULT FALSE,
-	dateOfCreation DATETIME DEFAULT NOW(), # account creation date
-    lastLoginDate DATETIME NULL, # date of last login
-    #lastLoginAttempt DATETIME NULL, # date of any login attempt, successful or not
-    #failedLogins BIGINT(20) NULL, # reset on successful login
-    #lastLoginAddress VARCHAR(64) NULL, # last known ip
-    sessionIP BLOB NULL, # 
-    sessionExpirationDate DATETIME NULL,
-    sessionToken BLOB NULL, # 
-	PRIMARY KEY (id)
+	username VARCHAR(18) NOT NULL,
+	combinedHash BLOB NOT NULL, # result of user input and salt
+	salt BLOB NOT NULL, # a random series of bytes
+    email VARCHAR(128) NOT NULL,
+    permission INT DEFAULT 0,
+    locked BOOL DEFAULT FALSE, 
+	dateOfCreation DATETIME DEFAULT NOW(),
+    lastLoginDate DATETIME NULL, # set on token generation and consumption
+    sessionIP BLOB NULL, # set on token generation and consumption
+    sessionExpirationDate DATETIME NULL, # set on token generation and set to null on consumption
+    sessionToken BLOB NULL, # set on token generation and set to null on consumption
+	PRIMARY KEY (id),
+    UNIQUE KEY (username),
+    UNIQUE KEY (email)
 );
+
 CREATE TABLE IF NOT EXISTS useraccountrecovery (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT,
 	token BLOB NOT NULL, 
@@ -60,22 +64,3 @@ CREATE TABLE IF NOT EXISTS useraccountrecovery (
     
     PRIMARY KEY (id)
 );
-
-CREATE TABLE IF NOT EXISTS activesessions (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    useraccountid BIGINT(20) NOT NULL, # useraccounts.id
-    token BLOB NOT NULL, # generated on login
-    dateOfCreation DATETIME DEFAULT NOW(),
-    
-    active BOOL DEFAULT TRUE,
-    
-    PRIMARY KEY (id),
-    FOREIGN KEY (useraccountid) REFERENCES useraccounts(id)
-);
-
-
-# user access to our stored functions and procedures
-#GRANT EXECUTE ON FUNCTION wss_debug.WSService_MakeHash TO 'web'@'%';
-#REVOKE EXECUTE ON FUNCTION wss_debug.WSService_MakeHash FROM 'web'@'%';
-
-#REVOKE EXECUTE ON PROCEDURE wss_debug.WSProc_SelectUserAccountOnAuth FROM 'web'@'%';
