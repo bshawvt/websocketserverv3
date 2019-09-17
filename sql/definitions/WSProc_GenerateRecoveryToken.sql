10fbf44e8113a725a2730d233398100d12f0db0a
bshaw <bshawvt@gmail.com>
2019-09-17 13:10:25 -0400

    recovery token generation

--- sql/definitions/WSProc_GenerateRecoveryToken.sql
+++ sql/definitions/WSProc_GenerateRecoveryToken.sql
@@ -1,5 +1,8 @@
-CREATE PROCEDURE `WSProc_GenerateRecoveryToken` (IN email VARCHAR(128))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `WSProc_GenerateRecoveryToken`(IN email VARCHAR(128))
 BEGIN
-	INSERT INTO useraccountrecovery (token, owner_id)
-	SELECT WSService_MakeHash(TO_BASE64(RANDOM_BYTES(24)), email), (SELECT t1.user_id FROM useraccounts AS t1 WHERE t1.email = email);
+	SET @token = WSService_MakeHash(TO_BASE64(RANDOM_BYTES(24)), email);
+	INSERT INTO useraccountrecovery (token, owner_id, recoveryExpirationDate)
+	SELECT @token, (SELECT t1.user_id FROM useraccounts AS t1 WHERE t1.email = email), TIMESTAMPADD(HOUR, 2, NOW());
+    
+    SELECT @token AS recoveryToken;
 END
